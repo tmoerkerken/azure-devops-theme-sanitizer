9c6b2ac4151442400f058e25c4b085a250f29fd0
Tim Moerkerken <tim@clinicalgraphics.com>
2024-03-11 10:31:13 +0100

    Update folder structure and cleaned up styling

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# azure-devops-clipboard-sanitizer
+# Azure DevOps Theme Sanitizer
+
+If you use a dark theme in Azure DevOps, you potentially render fields illegible for your fellow light theme coworkers after copying over text from other fields.
+This plugin sanitizes theme related code when copying text on Azure DevOps, which should fix most of the legibility problems.
+
+![alt text](images/screenshot.png)
+
+Plugin is in very early development, so stay tuned for updates!
+
+Published at [Google Web store](https://chromewebstore.google.com/detail/azure-devops-clipboard-sa/eoghblbpfpainiedalioipimndfdkipe?hl=en-US&utm_source=ext_sidebar).
+
--- extension/extension.js
+++ extension/extension.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Azure DevOps Clipboard Sanitizer
-// @namespace    https://github.com/tmoerkerken
-// @match        https://dev.azure.com/*
-// @version      2024-03-07
-// @description  Implements workaround for https://developercommunity.visualstudio.com/t/text-copied-pasted-from-dark-mode-causes-the-text/1472018
-// @author       You
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none
-// ==/UserScript==
-
 const getHTMLFromClipboardContents = async (clipboardContents) => {
   const blobs = { 'text/html': null, 'text/plain': null };
   for (const item of clipboardContents) {
@@ -28,16 +17,13 @@ const getHTMLFromClipboardContents = async (clipboardContents) => {
 }
 
 const spawnNotification = (count) => {
-  console.log('Spawning notification');
   document.getElementById("sanitizerCount").innerText = count;
   document.getElementById("clipboardSanitizerNotification").style.display = 'inline';
   setTimeout(() => { document.getElementById("clipboardSanitizerNotification").style.display = 'none' }, 2000);
 }
 
 const sanitizeClipboard = async () => {
-  const clipboard = await navigator.clipboard.read(
-    { unsanitized: 'text/html' },
-  );
+  const clipboard = await navigator.clipboard.read();
   let result = await getHTMLFromClipboardContents(clipboard);
 
   if (!result['text/html']) {
@@ -57,36 +43,41 @@ const sanitizeClipboard = async () => {
   }
 }
 
-(() => {
-  'use strict';
-  document.getElementsByTagName('body')[0].insertAdjacentHTML(
-    'beforeBegin', `<div id="clipboardSanitizerNotification"
-           style="
-             position:fixed;
-             left:0;
-             right:0;
-             bottom: 0;
-             z-index: 9999999;
-             padding: 1rem 2rem;
-             background: #7fb76f;
-             border-radius: 0.5rem;
-             font-family: Arial;
-             box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.5);
-             margin: 3% auto;
-             max-width: 500px;
-             text-align: center;
-             display: none;
-             ">
-               <span id="sanitizerCount">x</span> theme related problem(s) were fixed in the clipboard!
-         </div>`
-  );
-  console.log("Azure DevOps Clipboard Sanitizer loaded!")
+const styles = /*css*/`
+  #clipboardSanitizerNotification {
+    position:fixed;
+    left:0;
+    right:0;
+    bottom: 0;
+    z-index: 9999999;
+    padding: 1rem 2rem;
+    background: #7fb76f;
+    border-radius: 0.5rem;
+    font-family: Arial;
+    box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.5);
+    margin: 3% auto;
+    max-width: 500px;
+    text-align: center;
+    display: none;
+  }
+`;
 
-  window.addEventListener('keydown', function (e) {
-    if ((e.metaKey || e.ctrlKey) && e.keyCode == 67) {
-      // Command/Ctrl + c pressed
-      sanitizeClipboard();
-    }
-  });
+document.getElementsByTagName('body')[0].insertAdjacentHTML(
+  'beforeBegin', `
+    <div id="clipboardSanitizerNotification">
+      <span id="sanitizerCount">x</span> theme related problem(s) were fixed in the clipboard!
+    </div>
+  `
+);
+console.log("Azure DevOps Clipboard Sanitizer loaded!")
 
-})();
+window.addEventListener('keydown', function (e) {
+  if ((e.metaKey || e.ctrlKey) && (e.key === "c" || e.key === "x")) {
+    // Command/Ctrl + c/x pressed
+    sanitizeClipboard();
+  }
+});
+
+const styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
